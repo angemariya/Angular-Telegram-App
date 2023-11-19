941b1edc52cbeb15711bb821da6c9eec435e5ada
Maria Angelova <angelova.m@hotmail.com>
2023-11-19 21:22:51 +0100

    feat: modify shop component

--- src/app/pages/shop/shop.component.ts
+++ src/app/pages/shop/shop.component.ts
@@ -1,14 +1,33 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component, inject} from '@angular/core';
+import {TelegramService} from "../../services/telegram.service";
+import { ProductsService } from '../../services/products.service';
+import { ProductListComponent } from '../../components/product-list/product-list.component';
 
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [CommonModule],
+  imports: [ProductListComponent],
   template: `
-    <h1>main shop</h1>
+    <app-product-list
+      [title]="'Luxury cars'"
+      [products]="products.byGroup['cars']"
+    />
+    <app-product-list
+      [title]="'Clothing'"
+      [products]="products.byGroup['clothing']"
+    />
+    <app-product-list
+      [title]="'Jewelry'"
+      [products]="products.byGroup['jewelry']"
+    />
   `
 })
+  
 export class ShopComponent {
+  telegram = inject(TelegramService);
+  products = inject(ProductsService);
 
+  constructor() {
+    this.telegram.BackButton.hide();
+  }
 }
